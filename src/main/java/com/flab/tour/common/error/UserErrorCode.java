6c08644bc4ae5163273621682229773be9863ca7
kps990515 <31429892+kps990515@users.noreply.github.com>
2024-04-28 23:02:47 +0900

    #8 RequestBody valid에러 처리

--- src/main/java/com/flab/tour/common/error/UserErrorCode.java
+++ src/main/java/com/flab/tour/common/error/UserErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum UserErrorCode implements BaseErrorCode {
     USER_NOT_FOUND(400, 1001, "사용자를 찾을 수 없음"),
     REGISTER_FAIL(400, 1002, "가입 처리 실패"),
-    EXIST_ID(200, 1003, "존재하는 ID");
+    EXIST_ID(200, 1003, "존재하는 ID"),
+    VALIDATION_ERROR(400, 1004, "입력값이 유효하지 않음");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
