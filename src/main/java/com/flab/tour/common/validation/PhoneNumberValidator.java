1b4fecf31e96e3983c6075f7a28f01326b964f30
kps990515 <31429892+kps990515@users.noreply.github.com>
2024-04-28 21:55:33 +0900

    #6 Entity 연결

---
+++ src/main/java/com/flab/tour/common/validation/PhoneNumberValidator.java
@@ -0,0 +1,20 @@
+package com.flab.tour.common.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
+    private String regexp;
+
+    @Override
+    public void initialize(PhoneNumber constraintAnnotation) {
+        regexp = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return Pattern.matches(regexp, value);
+    }
+}
--- src/main/java/com/flab/tour/domain/user/service/UserService.java
+++ src/main/java/com/flab/tour/domain/user/service/UserService.java
@@ -1,21 +1,39 @@
 package com.flab.tour.domain.user.service;
 
+import com.flab.tour.common.error.UserErrorCode;
+import com.flab.tour.common.exception.ApiException;
 import com.flab.tour.config.objectmapper.BaseService;
 import com.flab.tour.db.user.UserEntity;
+import com.flab.tour.db.user.UserMapper;
+import com.flab.tour.db.user.UserRepository;
 import com.flab.tour.domain.user.controller.model.UserLoginRequest;
-import com.flab.tour.domain.user.controller.model.UserLoginResponse;
+import com.flab.tour.domain.user.controller.model.UserRegisterRequest;
+import com.flab.tour.domain.user.controller.model.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService extends BaseService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserLoginResponse login(UserLoginRequest svo) {
-        var userEntity = copyVO(svo, UserEntity.class);
-        var userResponse = copyVO(svo, UserLoginResponse.class);
+    public UserResponse login(UserLoginRequest request) {
+        var userEntity = userRepository.findFirstByEmailAndPasswordOrderByUserIdDesc(request.getEmail(), request.getPassword())
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
 
-        // TODO 토큰생성
-        return (UserLoginResponse) userResponse;
+        return new UserResponse("로그인완료");
+    }
+
+    public UserResponse register(UserRegisterRequest svo) {
+        userRepository.findFirstByEmail(svo.getEmail())
+                .ifPresent(user -> {
+                    throw new ApiException(UserErrorCode.EXIST_ID);
+                });
+
+        UserEntity newUser = userMapper.toUserEntity(svo);
+        userRepository.save(newUser);
+
+        return new UserResponse("가입완료");
     }
 }
