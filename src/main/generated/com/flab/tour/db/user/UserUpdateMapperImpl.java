2b7d9898b08254965bbff64fa5e17ad65d7ab948
kps990515 <31429892+kps990515@users.noreply.github.com>
2024-05-16 23:00:42 +0900

    #26 이슈 011 - 테스트코드작성

---
+++ src/main/generated/com/flab/tour/db/user/UserUpdateMapperImpl.java
@@ -0,0 +1,31 @@
+package com.flab.tour.db.user;
+
+import com.flab.tour.domain.user.controller.model.UserUpdateRequest;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-16T22:14:33+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class UserUpdateMapperImpl implements UserUpdateMapper {
+
+    @Override
+    public void updateUserFromRequest(UserUpdateRequest request, UserEntity entity) {
+        if ( request == null ) {
+            return;
+        }
+
+        if ( request.getName() != null ) {
+            entity.setName( request.getName() );
+        }
+        if ( request.getPassword() != null ) {
+            entity.setPassword( request.getPassword() );
+        }
+        entity.setEmailMarketingConsent( request.isEmailMarketingConsent() );
+        entity.setSmsMarketingConsent( request.isSmsMarketingConsent() );
+        entity.setPushMarketingConsent( request.isPushMarketingConsent() );
+    }
+}
